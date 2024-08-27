c3326f9311c02879be9a6d6cafb937c13918d9f6
mohankoli02 <kolimohanlal02@gmail.com>
2024-08-27 12:43:52 +0530

    Update files

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -7,7 +7,7 @@ const router = express.Router();
 
 router.post("/register", singleUpload, register);
 router.post("/login", login);
-router.get("/logout", logout);
+router.get("/logout", isAuthenticated, logout);
 router.post("/profile/update", isAuthenticated,singleUpload,updateProfile);
 
 export default router;
